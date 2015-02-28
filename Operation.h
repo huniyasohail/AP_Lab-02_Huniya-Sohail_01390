13bff96ee6748c42b9d94ccdd13632ca88ada282
huniyasohail <huniyasohail1@gmail.com>
2015-02-28 23:03:11 +0500

    Lab02_final
    Advanced Programming

--- Operation.h
+++ Operation.h
@@ -4,43 +4,30 @@
 #include <string>
 #include <ctype.h>
 using namespace std;
+int checkflag = 0;
+int cot = 0;
+float final_res = 0;
+int no_of_sym = 0;
+int no_of_var = 0;
+char * sym[100];
+char * svar[100];
 
-int addition(int a, int b){//when + operator used then call function
-	int c=a + b;
-	cout<<"\n\n***sum is:"<<c<<"*********"<<endl;
-	return c ;
-}
-int Subtraction(int a, int b){//when - operator used then call function
-	int c=a - b;
-	cout<<"\n\n***Difference is:"<<c<<"*********"<<endl;
-	return c ;
-}
-int division(int a, int b){//when / operator used then call function
-	int c=a / b;
-	cout<<"\n\n***result is:"<<c<<"*********"<<endl;
-	return c ;
-}
-int multiplication(int a, int b){//when * operator used then call function
-	int c=a * b;
-	cout<<"\n\n***multiply is:"<<c<<"*********"<<endl;
-	return c ;
-}
-class Pair{//Class Declaration
+template <class A_Type=A_Type, class B_Type=B_Type> class Pair {//Class Declaration
 private:
-	string key;//private attributes
-	int value;
+	 A_Type key;//private attributes
+	 B_Type value;
 public:
 	
-	Pair(){}//constructor
-	Pair(string k, int v){
+	Pair(){};//constructor
+	Pair(A_Type k, B_Type v){
 		key = k;
 		value = v;
 	}
 	//getter and setter
-	int getValue(){ return value; }
-	string getKey(){ return key; }
-	void setValue(int v){ value = v; }
-	void setKey(string k){ key = k; }
+	B_Type getValue(){ return value; }
+	A_Type getKey(){ return key; }
+	void setValue(B_Type v){ value = v; }
+	void setKey(A_Type k){ key = k; }
 
 	//++ operator
 	Pair operator+(Pair var){
@@ -50,6 +37,29 @@ public:
 		return temp;
 
 	}
+	B_Type addition(B_Type a, B_Type b){//when + operator used then call function
+	B_Type c=a + b;
+	cout<<"\n\n***sum is:"<<c<<"*********"<<endl;
+	return c ;
+}
+	B_Type division(B_Type a, B_Type b){//when / operator used then call function
+		B_Type c;
+		if(b!=0){
+	c=a / b;
+		cout<<"\n\n***result is:"<<c<<"*********"<<endl;
+		return c ;
+		}
+		else{
+			cout << "can not divide by zero\n";
+			return c ;
+		}
+	
+}
+	B_Type multiplication(B_Type a, B_Type b){//when * operator used then call function
+	B_Type c=a * b;
+	cout<<"\n\n***multiply is:"<<c<<"*********"<<endl;
+	return c ;
+}
 	//assignment operator
 	Pair operator=(Pair var){
 		key = var.key;
@@ -61,4 +71,95 @@ public:
 		cout << "the value of Key is" << value;
 		cout << endl;
 	}
+	B_Type Subtraction(B_Type a, B_Type b){//when - operator used then call function
+	B_Type c=a - b;
+	cout<<"\n\n***Difference is:"<<c<<"*********"<<endl;
+	return c ;
+}
 };
+
+void CheckingLength(char*s[], int x2){//measure the length of the expression in order to check the syntax
+
+
+	int k = 0;
+	int l = 0;
+	for (int i = 0; i < x2; i++)//loop to count the variable and operators
+	{
+		if ((strcmp(s[i], "+") == 0) || (strcmp(s[i], "-") == 0) || (strcmp(s[i], "/") == 0) || (strcmp(s[i], "*") == 0))
+		{
+			no_of_sym++;//increment if operator found
+			sym[k] = s[i];
+			k++;//track to count the operator
+
+		}
+		else
+		{
+			no_of_var++;//if variable found increment it
+			svar[l] = s[i];
+			l++;
+		}
+	}
+
+	if (no_of_sym == no_of_var - 1)//if lenght of variable is less then the operator then fine
+	{
+
+	}
+	else
+	{
+		std::cout << "syntax error\n";
+		checkflag = 1;
+	}
+
+}
+
+void SytaxErrorCheck(string s)
+{
+	char s2[100];
+	char s23[100];
+	char *str3[100];
+	char * pch1;
+	const char * cs;
+	int x = 0;
+	cs = s.c_str();
+	strcpy(s2, cs);
+	pch1 = strtok(s2, "  ;");
+	str3[x] = pch1;
+	//const char * a;
+	x++;
+	while (pch1 != NULL)
+	{
+		pch1 = strtok(NULL, "  ;");
+		str3[x] = pch1;
+		x++;
+	} // end of while
+
+
+	char str[] = " ";
+
+	for (int i = 0; i < x - 1; i++)
+	{
+
+
+		if (strcmp(str3[i], "=") == 0)
+		{
+			str3[i - 1]; //should be a variable name
+			strcpy(s23, str3[i - 1]);
+			if (isdigit(s23[0]))
+			{
+				std::cout << "Syntax error\n";
+				checkflag = 1;
+			}
+
+			if (str3[i + 1] == NULL)
+			{
+				std::cout << "Syntax error\n";
+				checkflag = 1;
+			}
+
+		}
+
+	}
+
+
+}
+
--- newInterpretor.cpp
+++ newInterpretor.cpp
@@ -1,10 +1,12 @@
 #include"Operation.h"
+
 int main()
 {
 	//local variable declaration
 	string line;
-	Pair *obj1 = new Pair[50];
-	Pair ob2("ji", 4);
+	Pair <string,int>*obj1 = new Pair<string,int>[50];
+	Pair <string,int> obj2;
+	//Pair ob2("ji", 4);
 	char str[80];
 	string app[11];
 	char arr[80];
@@ -14,10 +16,10 @@ int main()
 	int i = 0;
 	int chk1 = 0, chk2;
 	int n = 0;
-	int result;
+	 int result;
 	int limit = 0;
 	int q = 0;
-	char ch1[50], ch2[50];
+	 char ch1[50], ch2[50];
 	int ob = 0;
 	//y.display();
 	//cout<<"The value of y is ",y.getValue() <<endl;
@@ -30,6 +32,13 @@ int main()
 		{
 			//copy entire line into another string
 			strcpy_s(str, line.c_str());
+			SytaxErrorCheck(str);
+		if (checkflag == 1)
+		{
+			getchar();
+			getchar();
+			return 0;
+		}
 
 			ptr = &str[0];//get starting address of copied string
 			cout << "line :" << line << '\n';
@@ -38,6 +47,8 @@ int main()
 			for (int k = 0; k<11; k++){
 				app[k] = "";
 			}
+
+
 			//to eliminate the deliminitor
 			while (j<line.length()){
 				if (*(ptr + j) != ' '){//if no sapce then execute
@@ -70,7 +81,7 @@ int main()
 					if(chk1==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch1){//if found in pair class then get the value of the particular key
-								chk1=obj1[uo].getValue();
+								chk1=(obj1[uo].getValue());
 								va=1;//increment the value to track the value exist
 							}
 							
@@ -82,7 +93,7 @@ int main()
 						if(chk2==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch2){//if found in pair class then get the value of the particular key
-								chk2=obj1[uo].getValue();
+								chk2=(obj1[uo].getValue());
 								vb=1;//increment the value to track the value exist
 							}
 						}
@@ -93,7 +104,7 @@ int main()
 					
 						if(chk1!=0 && chk2!=0){//check if both numbers are int then do this
 							
-							result=addition(chk1,chk2);
+							result=obj2.addition(chk1,chk2);
 							app[n-2]=to_string(result);
 							//	cout<<"\nThe sum is :"<< app[n+1]<<endl;
 								n=n - 1;//decrement the value
@@ -112,7 +123,7 @@ int main()
 					if(chk1==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch1){//if found in pair class then get the value of the particular key
-								chk1=obj1[uo].getValue();
+								chk1=(obj1[uo].getValue());
 								va=1;
 							}
 							
@@ -124,7 +135,7 @@ int main()
 						if(chk2==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch2){//if found in pair class then get the value of the particular key
-								chk2=obj1[uo].getValue();
+								chk2=(obj1[uo].getValue());
 								vb=1;
 							}
 						}
@@ -135,7 +146,7 @@ int main()
 					
 						if(chk1!=0 && chk2!=0){
 							
-							result=multiplication(chk1,chk2);
+							result=obj2.multiplication(chk1,chk2);
 							app[n-2]=to_string(result);
 							//	cout<<"\nThe sum is :"<< app[n+1]<<endl;
 								n=n - 1;
@@ -155,7 +166,7 @@ int main()
 					if(chk1==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch1){//if found in pair class then get the value of the particular key
-								chk1=obj1[uo].getValue();
+								chk1=(obj1[uo].getValue());
 								va=1;
 							}
 							
@@ -167,7 +178,7 @@ int main()
 						if(chk2==0){//if string is not int then return 0
 						for(int uo=0;uo<limit;uo++){
 							if(obj1[uo].getKey()==ch2){//if found in pair class then get the value of the particular key
-								chk2=obj1[uo].getValue();
+								chk2=(obj1[uo].getValue());
 								vb=1;
 							}
 						}
@@ -178,7 +189,7 @@ int main()
 					
 						if(chk1!=0 && chk2!=0){
 							
-							result=division(chk1,chk2);
+							result=obj2.division(chk1,chk2);
 							app[n-2]=to_string(result);
 							//	cout<<"\nThe sum is :"<< app[n+1]<<endl;
 								n=n - 1;
@@ -237,19 +248,22 @@ int main()
 			}
 			q = 0;
 			int jo=0;
+			cout<<"ggg value"<<app[0]<<endl;
 			if(app[0]=="print"){//if print keyword found
 				for(int ok=0;ok<ob;ok++)
 				{
+					
 					if(app[1]==obj1[ok].getKey()){//find the particular ky to get the value
-					obj1[ok].display();
-					jo++;
+						//cout<<"key :" <<obj1[ok].getKey()<<" Value :"<<obj1[ok].getValue()<<endl;
+						obj1[ok].display();
+						jo++;
 					}
 				}
 				if(jo==0){
 				cout<<"Key Value not exist!!!"<<endl;
 				}
 			}
-			cout << "str :" << str << '\n';
+		//	cout << "str :" << str << '\n';
 		}
 		myfile.close();//close the file
 	}
